ac6934dfd6873f093047f16941d10c41161dbe61
JasonMorris1 <jasonbm123@gmail.com>
2023-01-19 13:25:52 -0800

    added tests

--- app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
+++ app/src/test/java/edu/ucsd/cse110/lab2/UtilitiesTest.java
@@ -1,2 +1,14 @@
-package edu.ucsd.cse110.lab2;public class UtilitiesTest {
+package edu.ucsd.cse110.lab2;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class UtilitiesTest {
+    @Test
+    public void test_TrimDisplayStr(){
+        assertEquals("1.234", Utilities.trimDisplayStr("1.2340"));
+        assertEquals("13", Utilities.trimDisplayStr("13.0"));
+    }
 }
